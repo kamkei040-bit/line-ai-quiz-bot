4c7e8845bd2908b97e9a07a9127cc3c38ec7b59d
kamkei040-bit <kamkei040@gmail.com>
2026-03-15 08:45:06 +0900

    Update app.py
    update callback

--- app.py
+++ app.py
@@ -12,13 +12,17 @@ CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
+
 @app.route("/")
 def home():
     return "LINE BOT OK"
 
+
 @app.route("/callback", methods=['POST'])
 def callback():
+
     signature = request.headers['X-Line-Signature']
+
     body = request.get_data(as_text=True)
 
     try:
@@ -29,8 +33,10 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
+
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
 
@@ -40,6 +46,7 @@ def handle_message(event):
                 messages=[TextMessage(text="テスト返信です")]
             )
         )
+
 
 if __name__ == "__main__":
     app.run()
